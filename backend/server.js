fedaad05c896b6a240b0872593fb4f820cad1f66
jesusJimenezJ <orquideawellness@gmail.com>
2026-06-29 00:11:17 -0500

    Agregando helmet para seguridad

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const path = require("path");
 
 const app = express();
 
+// Configuración necesaria para trabajar detrás del proxy de Render
+app.set('trust proxy', 1);
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,10 +16,19 @@ app.use(express.json());
 const iaRoutes = require("./routes/ia.routes");
 app.use("/api/ia", iaRoutes);
 
-// 👉 SERVIR FRONTEND (HTML fuera del backend)
+// 👉 SERVIR FRONTEND (Archivos HTML/CSS/JS fuera de la carpeta backend)
 app.use(express.static(path.join(__dirname, "../")));
 
+// RUTA COMODÍN (SPA)
+// Asegura que si el usuario navega a una URL interna del frontend, 
+// el servidor siempre entregue el index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
 // PORT
+// Render inyecta automáticamente la variable de entorno PORT. 
+// Si no, el servidor usará el puerto 4000 localmente.
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
